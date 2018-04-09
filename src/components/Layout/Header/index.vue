646757892ff1dfea2d64cb1dda1120114042b233
onwl007 <onwl007@126.com>
2018-04-09 20:58:46 +0800

    header

---
+++ src/components/Layout/Header/index.vue
@@ -0,0 +1,67 @@
+<template>
+  <el-header height="48px" class="header">
+    <a class="collapse" @click="handleToggleCollapseMenue">
+      <i class="iconfont" :class="[`icon-${asideCollapse ? 'unfold' : 'fold'}`]"></i>
+    </a>
+    <div class="action">
+      <router-link to="/" class="action-item message">
+        <el-badge is-dot>
+          <i class="iconfont icon-message"></i>
+        </el-badge>
+      </router-link>
+      <el-menu class="user" mode="horizontal" @select="handleMenuSelect">
+        <el-submenu index="submenu">
+          <template slot="title">
+            <i class="iconfont icon-user"></i>
+            <span>{{authInfo.name}}</span>
+          </template>
+          <el-menu-item index="info">
+            <i class="iconfont icon-user-info"></i>
+            <span>个人信息</span>
+          </el-menu-item>
+          <el-menu-item index="logout">
+            <i class="iconfont icon-logout"></i>
+            <span>退出</span>
+          </el-menu-item>
+        </el-submenu>
+      </el-menu>
+    </div>
+  </el-header>
+</template>
+
+<script>
+import { mapGetters } from 'vuex'
+export default {
+  name: 'Layout-Header',
+  data () {
+    return {}
+  },
+  computed: {
+    ...mapGetters({
+      asideCollapse: 'app/asideCollapse',
+      authInfo: 'auth/info'
+    })
+  },
+  methods: {
+    handleToggleCollapseMenu () {
+      this.$store.commit('app/SET_ASIDE_COLLAPSE', !this.asideCollapse)
+    },
+    async handleMenuSelect (index) {
+      switch (index) {
+        case 'info':
+          this.$router.push({ name: 'Auth' })
+          break
+        case 'logout':
+          await this.$store.dispatch('auth/logout')
+          this.$router.push({ name: 'Login' })
+          break
+        default:
+          break
+      }
+    }
+  }
+}
+</script>
+
+<style lang="stylus">
+</style>
