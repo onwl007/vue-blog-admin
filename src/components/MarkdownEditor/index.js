d68e3e007ca43a7d2078b9a05110760d44699a78
onwl007 <onwl007@126.com>
2018-04-10 22:19:15 +0800

    MarkdownEditor

---
+++ src/components/MarkdownEditor/index.js
@@ -0,0 +1,12 @@
+/**
+ * @desc MarkdownEditor Entry
+ */
+
+'use strict'
+
+import MarkdownEditor from './index.vue'
+import commands from './lib/commands'
+
+MarkdownEditor.commands = commands
+
+export default MarkdownEditor
