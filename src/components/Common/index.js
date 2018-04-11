1e4de7ea1f66eb3a3cb411379a5ae60527799fa4
onwl007 <onwl007@126.com>
2018-04-11 21:12:46 +0800

    index

---
+++ src/components/Common/index.js
@@ -0,0 +1,13 @@
+/**
+ * @desc Common components entry
+ */
+'use strict'
+
+export { default as ActionButtons } from './ActionButtons'
+export { default as ArticleDetail } from './ArticleDetail'
+export { default as ArticlesList } from './ArticlesList'
+export { default as FormEdit } from './FormEdit'
+export { default as Guide } from './Guide'
+export { default as MarkdeonEditor } from './MarkdownEditor'
+export { default as PageLoading } from './PageLoading'
+export { default as Uploader } from './Uploader'
