0837f0bc45c44a2c56bb1b0571da659ac5dd3125
onwl007 <onwl007@126.com>
2018-04-11 21:14:30 +0800

    index

---
+++ src/components/Layout/index.js
@@ -0,0 +1,9 @@
+/**
+ * @desc Layout components entry
+ */
+
+'use strict'
+
+export { default as AppHeader } from './Header'
+export { default as AppAside } from './Aside'
+export { default as AppFooter } from './Footer'
