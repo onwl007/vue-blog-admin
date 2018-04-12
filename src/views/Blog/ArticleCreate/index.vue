33456980a2a2a4c29c7b74d48a9e51d78c54f277
onwl007 <onwl007@126.com>
2018-04-12 21:26:49 +0800

    ArticleCreate

---
+++ src/views/Blog/ArticleCreate/index.vue
@@ -0,0 +1,47 @@
+<template>
+  <section class="blog-article-create-page">
+    <ArticleDetail :article-model="model" @submit="handleCreate"></ArticleDetail>
+  </section>
+</template>
+
+<script>
+import { ArticleDetail } from '@/components/Common'
+export default {
+  name: 'Blog-ArticleCreate',
+  components: {
+    ArticleDetail
+  },
+  data () {
+    return {
+      model: null
+    }
+  },
+  created () {
+    this.buildModel()
+  },
+  methods: {
+    buildModel () {
+      this.model = {
+        title: '',
+        description: '',
+        state: 0,
+        keywords: [],
+        tag: [],
+        permailink: '',
+        thumb: '',
+        content: ''
+      }
+    },
+    handleCreate (model) {
+      this.$store.dispatch('article/create', model).then(success => {
+        if (success) {
+          this.$router.push({ name: 'Blog - Articles' })
+        }
+      })
+    }
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+</style>
