e2ff2fd752535b20605950c5e74ed7d7d1566084
onwl007 <onwl007@126.com>
2018-04-15 16:26:39 +0800

    App

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,91 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <router-view/>
+    <PageLoading v-if="authLoading"></PageLoading>
+    <template v-else>
+      <transition name="fade" mode="out-in">
+        <router-view v-if="fullscreen"></router-view>
+        <el-container class="app-container" v-else>
+          <AppAside></AppAside>
+          <el-container style="overflow: hidden" direction="vertical">
+            <AppHeader></AppHeader>
+            <el-main class="app-main" @scroll.native.stop.prevent="handleScroll">
+              <Guide class="app-guide" v-if="showGuide"></Guide>
+              <transition name="fade" mode="out-in">
+                <router-view></router-view>
+              </transition>
+            </el-main>
+            <AppFooter></AppFooter>
+          </el-container>
+        </el-container>
+      </transition>
+    </template>
+    <ActionButtons></ActionButtons>
   </div>
 </template>
 
 <script>
+import { mapGetters } from 'vuex'
+import { AppHeader, AppAside, AppFooter } from '@/components/Layout'
+import { PageLoading, ActionButtons, Guide } from '@/components/Common'
+
 export default {
-  name: 'App'
+  name: 'App',
+  components: {
+    AppHeader,
+    AppAside,
+    AppFooter,
+    PageLoading,
+    ActionButtons,
+    Guide
+  },
+  data () {
+    return {
+      goToTopThreshold: 200
+    }
+  },
+  computed: {
+    ...mapGetters({
+      authLoading: 'auth/loading',
+      actionButtonVisible: 'app/actionButtonVisible'
+    }),
+    fullscreen () {
+      return this.$route.meta.fullscreen
+    },
+    showGuide () {
+      return !['Home'].includes(this.$route.name)
+    }
+  },
+  methods: {
+    handleScroll (e) {
+      const container = e.target
+      const showGoToTop = container.scrollTop > this.goToTopThreshold
+      const payload = {}
+      if (showGoToTop && !this.actionButtonVisible.goToTop) {
+        payload.goToTop = true
+      } else if (!showGoToTop && this.actionButtonVisible.goToTop) {
+        payload.goToTop = false
+      }
+      if (Object.keys(payload).length) {
+        this.$store.commit('app/SET_ACTION_BUTTON_VISIBLE', payload)
+      }
+    }
+  }
 }
 </script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+<style lang="stylus" scoped>
+#app
+.app-container {
+  width: 100%;
+  height: @width;
+}
+
+.app-main {
+  position: relative;
+  padding: 24px;
+}
+
+.app-guide {
+  margin-bottom: 24px;
 }
 </style>
